aa4356ea6a160270898257b321219e3ad753638c
thashreef <chthashreef22@gmail.com>
2025-07-03 01:17:32 +0530

    folder structure

--- frontend-parlour-dashboard/src/app/dashboard/page.tsx
+++ frontend-parlour-dashboard/src/app/dashboard/page.tsx
@@ -1,5 +1,14 @@
+import { Sidebar } from "@/components";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="flex w-full h-[100vh]">
+      <aside className="flex-1 h-full bg-red-400">
+        <Sidebar />
+      </aside>
+      <main className="flex-3 h-full bg-yellow-900">content here</main>
+    </div>
+  );
 };
 
 export default Dashboard;
